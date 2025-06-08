15f7cc5e402181b0610593449a3ffcdce980bcb3
Abdukodir <abdukodirarifzanov@gmail.com>
2025-06-08 10:24:22 +0500

    added staticfiles

--- config/settings.py
+++ config/settings.py
@@ -137,5 +137,7 @@ STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = "/media/"
